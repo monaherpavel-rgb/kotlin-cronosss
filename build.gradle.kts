9fa2f1941635958ce4c97d769850b44c746dfe66
monaherpavel-rgb <monaherpavel@gmail.com>
2026-03-24 21:43:47 +0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+    id("com.google.dagger.hilt.android") version "2.51" apply false
+}
